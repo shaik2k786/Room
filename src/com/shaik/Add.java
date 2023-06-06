ea109b1bf38d8101c6024547a02fa0930a503211
Saidashaik786 <108187239+Saidashaik786@users.noreply.github.com>
2023-06-06 20:08:54 +0530

    room pratice

---
+++ src/com/shaik/Add.java
@@ -0,0 +1,11 @@
+package com.shaik;
+
+public class Add {
+	public void add() {
+		System.out.println("add method");
+	}
+public static void main(String[] args) {
+	Add a=new Add();
+	a.add();
+}
+}
